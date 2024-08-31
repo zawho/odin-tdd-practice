2b541356c887ff50e91b808b219e3a22d121d1bf
zawho <zthooper2@gmail.com>
2024-08-31 12:31:26 +0800

    Configure jest with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import jest from "eslint-plugin-jest";
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
+  jest,
 ];
