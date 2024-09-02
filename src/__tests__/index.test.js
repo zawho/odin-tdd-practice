649b3bf336b57fc2f6671c084c158d96961366fd
zawho <zthooper2@gmail.com>
2024-09-02 11:15:26 +0800

    Add test for z to a wrap in cipher

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -6,11 +6,11 @@ import {
 } from "../index.js";
 
 test('capitalizes "hello" as "Hello"', () => {
-	expect(capitalize("hello")).toBe("Hello");
+	expect(capitalize('hello')).toBe('Hello');
 });
 
 test('Reverse "flow" to "wolf"', () => {
-	expect(reverseString("flow")).toBe("wolf");
+	expect(reverseString('flow')).toBe('wolf');
 });
 
 test("Add 3 + 4 to equal 7", () => {
@@ -29,6 +29,14 @@ test("Divide 24 by 6 to equal 4", () => {
 	expect(calculator.divide(24, 6)).toBe(4);
 });
 
-test('Encrypt "hello" as "ifmmp"', () => {
-	expect(shiftCipher("hello", 1)).toBe("ifmmp");
+test('Encrypt "hello" as "ifmmp" with a key of 1', () => {
+	expect(shiftCipher('hello', 1)).toBe('ifmmp');
+});
+
+test('Encrypt "abc" as "def" with a key of 3', () => {
+  expect(shiftCipher('abc', 3)).toBe('def');
+});
+
+test('Encrypt "xyz" as "abc" with a key of 3', () => {
+  expect(shiftCipher('xyz', 3)).toBe('abc');
 });
